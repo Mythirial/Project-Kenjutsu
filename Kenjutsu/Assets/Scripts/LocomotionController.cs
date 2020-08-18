d02527f1c70c5b03412558ae9d5fa815ab869e07
Kevin Celinski <kevin.celinski@outlook.com>
2020-08-18 20:02:17 +0200

    :sparkles: Added Better Interactable Objects (#5)

--- Kenjutsu/Assets/Scripts/LocomotionController.cs
+++ Kenjutsu/Assets/Scripts/LocomotionController.cs
@@ -1,28 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class LocomotionController : MonoBehaviour
+namespace Assets.Scripts
 {
-    public XRController leftController;
-    public XRController rightController;
-    public InputHelpers.Button teleportActivateButton;
-    public float activationThreshold = 0.1f;
+    public class LocomotionController : MonoBehaviour
+    {
+        public XRController leftController;
+        public XRController rightController;
+        public InputHelpers.Button teleportActivateButton;
+        public float activationThreshold = 0.1f;
+
+        public bool EnableLeftTeleportation { get; set; } = true;
+        public bool EnableRightTeleportation { get; set; } = true;
+
     
-    // Update is called once per frame
-    void Update()
-    {
-        if(leftController)
-            leftController.gameObject.SetActive(CheckIfActivated(leftController));
+        // Update is called once per frame
+        private void Update()
+        {
+            if(leftController)
+                leftController.gameObject.SetActive(EnableLeftTeleportation && CheckIfActivated(leftController));
 
-        if (rightController)
-            rightController.gameObject.SetActive(CheckIfActivated(rightController));
-    }
+            if (rightController)
+                rightController.gameObject.SetActive(EnableRightTeleportation && CheckIfActivated(rightController));
+        }
 
-    public bool CheckIfActivated(XRController controller)
-    {
-        InputHelpers.IsPressed(controller.inputDevice, teleportActivateButton, out bool isPressed, activationThreshold);
-        return isPressed;
+        public bool CheckIfActivated(XRController controller)
+        {
+            controller.inputDevice.IsPressed(teleportActivateButton, out bool isPressed, activationThreshold);
+            return isPressed;
+        }
     }
 }
