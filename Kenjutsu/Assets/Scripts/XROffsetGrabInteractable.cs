6801860191dfc092d06777493834879ad82251f8
Kevin <kevin.celinski@outlook.com>
2020-08-18 20:00:30 +0200

    :sparkles: Added Better Interactable Objects

---
+++ Kenjutsu/Assets/Scripts/XROffsetGrabInteractable.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Assets.Scripts
+{
+    public class XROffsetGrabInteractable : XRGrabInteractable
+    {
+        private Vector3 _initialAttachmentLocalPos;
+        private Quaternion _initialAttachmentLocalRot;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            //Create Attachment point
+            if (!attachTransform)
+            {
+                GameObject grab = new GameObject("Grab Pivot");
+                grab.transform.SetParent(transform, false);
+                attachTransform = grab.transform;
+            }
+
+            _initialAttachmentLocalPos = attachTransform.localPosition;
+            _initialAttachmentLocalRot = attachTransform.localRotation;
+        }
+
+        protected override void OnSelectEnter(XRBaseInteractor interactor)
+        {
+            if (interactor is XRDirectInteractor)
+            {
+                attachTransform.position = interactor.transform.position;
+                attachTransform.rotation = interactor.transform.rotation;
+            }
+            else
+            {
+                attachTransform.localPosition = _initialAttachmentLocalPos;
+                attachTransform.localRotation = _initialAttachmentLocalRot;
+            }
+
+            base.OnSelectEnter(interactor);
+        }
+    }
+}
