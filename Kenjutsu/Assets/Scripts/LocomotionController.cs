4292d7e963ebd4c54aaa017b2e296076f6a4161b
Kevin Celinski <kevin.celinski@outlook.com>
2020-08-19 11:28:25 +0200

    :sparkles: Added Menu Interaction & Distance Grab (#6)
    Imported 
    - probuilder for future use
    - TMP

--- Kenjutsu/Assets/Scripts/LocomotionController.cs
+++ Kenjutsu/Assets/Scripts/LocomotionController.cs
@@ -10,6 +10,9 @@ namespace Assets.Scripts
         public InputHelpers.Button teleportActivateButton;
         public float activationThreshold = 0.1f;
 
+        public XRRayInteractor leftRayInteractor;
+        public XRRayInteractor righRayInteractor;
+
         public bool EnableLeftTeleportation { get; set; } = true;
         public bool EnableRightTeleportation { get; set; } = true;
 
@@ -17,11 +20,23 @@ namespace Assets.Scripts
         // Update is called once per frame
         private void Update()
         {
-            if(leftController)
-                leftController.gameObject.SetActive(EnableLeftTeleportation && CheckIfActivated(leftController));
+            Vector3 pos = new Vector3();
+            Vector3 norm = new Vector3();
+            int index = 0;
+            bool validTarget = false;
+
+            if (leftController)
+            {
+                bool isLeftRayInteractorHovering = leftRayInteractor.TryGetHitInfo(ref pos, ref norm, ref index, ref validTarget); 
+                leftController.gameObject.SetActive(EnableLeftTeleportation && CheckIfActivated(leftController) && !isLeftRayInteractorHovering);
+            }
+
 
             if (rightController)
-                rightController.gameObject.SetActive(EnableRightTeleportation && CheckIfActivated(rightController));
+            {
+                bool isRightRayInteractorHovering = righRayInteractor.TryGetHitInfo(ref pos, ref norm, ref index, ref validTarget);
+                rightController.gameObject.SetActive(EnableRightTeleportation && CheckIfActivated(rightController) && !isRightRayInteractorHovering);
+            }
         }
 
         public bool CheckIfActivated(XRController controller)
