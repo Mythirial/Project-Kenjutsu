993a2b0d7cbd650ad6dada6d6debe126cf48e96c
Kevin Celinski <kevin.celinski@outlook.com>
2020-08-20 12:23:57 +0200

    :sparkles: Added Ability to climb (#9)

---
+++ Kenjutsu/Assets/Scripts/ClimbingMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Assets.Scripts
+{
+    public class ClimbingMovement : MonoBehaviour
+    {
+        public static XRController climbingHand;
+
+        private CharacterController _character;
+        private ContinuousMovement _continuousMovement;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _character = GetComponent<CharacterController>();
+            _continuousMovement = GetComponent<ContinuousMovement>();
+        }
+
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            if (climbingHand)
+            {
+                _continuousMovement.enabled = false;
+                Climb();
+            }
+            else
+            {
+                _continuousMovement.enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// Climbing Computations
+        /// </summary>
+        private void Climb()
+        {
+            InputDevices.GetDeviceAtXRNode(climbingHand.controllerNode).TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 velocity);
+            _character.Move(transform.rotation * -velocity * Time.fixedDeltaTime);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Project-Kenjutsu
 A Unity VR Game
+
+This is the first VR game that im making.  
+the goal of this project is to make a VR Simulation of Samurai bamboo cutting.  
+
+while this may sound like a low bar I have more features in mind but for now the bare basics will have to be made first.  
+For Progress updates on the project Check the [blog].  
+
+[blog]: https://altair115.github.io
