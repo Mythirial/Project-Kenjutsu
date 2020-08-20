af3eba110a9c817b177c5bbb54e14628a0d2bc03
Kevin <kevin.celinski@outlook.com>
2020-08-20 12:22:27 +0200

    :sparkles: Added Ability to climb

---
+++ Kenjutsu/Assets/Scripts/ClimbInteractable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Assets.Scripts
+{
+    public class ClimbInteractable : XRBaseInteractable
+    {
+        protected override void OnSelectEnter(XRBaseInteractor interactor)
+        {
+            base.OnSelectEnter(interactor);
+            if(interactor is XRDirectInteractor)
+                ClimbingMovement.climbingHand = interactor.GetComponent<XRController>();
+        }
+
+        protected override void OnSelectExit(XRBaseInteractor interactor)
+        {
+            base.OnSelectExit(interactor);
+            if(interactor is XRDirectInteractor)
+                if (ClimbingMovement.climbingHand && ClimbingMovement.climbingHand.name == interactor.name)
+                    ClimbingMovement.climbingHand = null;
+        }
+    }
+}
