1b74fdbbda1ad2c52fc4d823ee0c0e8aa62ad40f
Kevin Celinski <kevin.celinski@outlook.com>
2020-08-24 13:11:04 +0200

    :sparkles: A VR Body (#13)
    * :sparkles: Added Upper Torso VR Full Body
    
    * :sparkles: Added Legs
    
    Made And Animated Legs As prep for hooking it up to the movement system
    
    * :sparkles: Added Animation Controller
    
    Full VR Body Without extra trackers done
    Setup for tracker is nearly done

---
+++ Kenjutsu/Assets/Scripts/FullbodyVRRig.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [System.Serializable]
+    public class VRMap
+    {
+        public Transform vrTarget;
+        public Transform rigTarget;
+        public Vector3 trackingPositionOffset;
+        public Vector3 trackingRotationOffset;
+
+        public void Map()
+        {
+            rigTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+            rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+        }
+    }
+
+    public class FullbodyVRRig : MonoBehaviour
+    {
+        [Header("Tracking Settings")] 
+        public float turnSmoothness = 3f;
+        public VRMap head;
+        public VRMap leftHand;
+        public VRMap rightHand;
+
+        [Header("Head Settings")]
+        public Transform headConstraint;
+        private Vector3 _headBodyOffset;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _headBodyOffset = transform.position - headConstraint.position;
+        }
+
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            transform.position = headConstraint.position + _headBodyOffset;
+            transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized, Time.fixedDeltaTime* turnSmoothness);
+
+            head.Map();
+            rightHand.Map();
+            leftHand.Map();
+        }
+    }
+}
---
+++ Kenjutsu/Assets/Scripts/VRFootIK.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRFootIK : MonoBehaviour
+{
+    [Range(0, 1)] public float rightFootPosWeight = 1f;
+    [Range(0, 1)] public float rightFootRotWeight = 1f;
+    [Range(0, 1)] public float leftFootPosWeight = 1f;
+    [Range(0, 1)] public float leftFootRotWeight = 1f;
+    public Vector3 footOffset;
+
+
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        Vector3 rightFootPos = _animator.GetIKPosition(AvatarIKGoal.RightFoot);
+        Vector3 leftFootPos = _animator.GetIKPosition(AvatarIKGoal.LeftFoot);
+        RaycastHit hit;
+
+        bool hasHit = Physics.Raycast(rightFootPos + Vector3.up, Vector3.down, out hit);
+        if (hasHit)
+        {
+            _animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, rightFootPosWeight);
+            _animator.SetIKPosition(AvatarIKGoal.RightFoot, hit.point + footOffset);
+
+            Quaternion rightFootRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, hit.normal), hit.normal);
+            _animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, rightFootRotWeight);
+            _animator.SetIKRotation(AvatarIKGoal.RightFoot, rightFootRotation);
+        }
+        else
+        {
+            _animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 0);
+        }
+
+        hasHit = Physics.Raycast(leftFootPos + Vector3.up, Vector3.down, out hit);
+        if (hasHit)
+        {
+            _animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, leftFootPosWeight);
+            _animator.SetIKPosition(AvatarIKGoal.LeftFoot, hit.point + footOffset);
+
+            Quaternion leftFootRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, hit.normal), hit.normal);
+            _animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, leftFootRotWeight);
+            _animator.SetIKRotation(AvatarIKGoal.LeftFoot, leftFootRotation);
+        }
+        else
+        {
+            _animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 0);
+        }
+
+    }
+    
+}
